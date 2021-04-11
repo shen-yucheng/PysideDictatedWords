29317c673ab33f2205437aa18d6e0cc7e24520db
shen2 <shen28311582@gmail.com>
2021-04-11 15:53:42 +0800

    精简代码，更改提示方式QLabel为setPlaceholderText，更改字体，更改边距

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ def start():
             "生成看音写词",
             "完成（关闭程序时才能写入文件夹）",
         )
-        messageBox.show()
     except Exception as error:
         messageBox = PySide2.QtWidgets.QMessageBox()
         messageBox.critical(
@@ -28,50 +27,33 @@ def start():
             "生成看音写词",
             str(error),
         )
-        messageBox.show()
 
 
 PySide2.QtCore.QCoreApplication.setAttribute(PySide2.QtCore.Qt.AA_EnableHighDpiScaling)
 app = PySide2.QtWidgets.QApplication(sys.argv)
 
 # 单个组件
-title_label = PySide2.QtWidgets.QLabel("标题：")
 title_entry = PySide2.QtWidgets.QLineEdit()
+title_entry.setPlaceholderText("标题")
 title_entry_layout = PySide2.QtWidgets.QHBoxLayout()
 title_entry_layout.addWidget(title_entry)
 title_entry_layout.addStretch()
-title_entry_frame = PySide2.QtWidgets.QWidget()
-title_entry_frame.setObjectName("title_entry_frame")
-title_entry_frame.setLayout(title_entry_layout)
-title_frame_layout = PySide2.QtWidgets.QVBoxLayout()
-title_frame_layout.addWidget(title_label)
-title_frame_layout.addWidget(title_entry_frame)
-title_frame = PySide2.QtWidgets.QWidget()
-title_frame.setLayout(title_frame_layout)
 
-content_label = PySide2.QtWidgets.QLabel("词语：")
 content_entry = PySide2.QtWidgets.QTextEdit()
-content_entry.setPlaceholderText("用空格分割")
-content_frame_layout = PySide2.QtWidgets.QVBoxLayout()
-content_frame_layout.addWidget(content_label)
-content_frame_layout.addWidget(content_entry)
-content_frame = PySide2.QtWidgets.QWidget()
-content_frame.setLayout(content_frame_layout)
+content_entry.setPlaceholderText("看音写词词语，用空格分割")
 
 start_button = PySide2.QtWidgets.QPushButton("启动")
+start_button.setObjectName("start_button")
 start_button.clicked.connect(start)
-button_frame_layout = PySide2.QtWidgets.QHBoxLayout()
-button_frame_layout.addStretch()
-button_frame_layout.addWidget(start_button)
-button_frame = PySide2.QtWidgets.QWidget()
-button_frame.setLayout(button_frame_layout)
+start_button_layout = PySide2.QtWidgets.QHBoxLayout()
+start_button_layout.addStretch()
+start_button_layout.addWidget(start_button)
 
 # 总体窗口布局
 layout = PySide2.QtWidgets.QVBoxLayout()
-
-layout.addWidget(title_frame)
-layout.addWidget(content_frame)
-layout.addWidget(button_frame)
+layout.addLayout(title_entry_layout)
+layout.addWidget(content_entry)
+layout.addLayout(start_button_layout)
 
 main_widget = PySide2.QtWidgets.QWidget()
 main_widget.setLayout(layout)
@@ -81,16 +63,16 @@ main_window.setWindowTitle("生成看音写词")
 main_window.setWindowIcon(
     PySide2.QtGui.QIcon("icon.svg")
 )
+
 desktop_widget = PySide2.QtWidgets.QDesktopWidget()
 main_window.resize(
     desktop_widget.width() * 0.7,
     desktop_widget.height() * 0.7
 )
-main_window.setCentralWidget(main_widget)
 
+main_window.setCentralWidget(main_widget)
 main_window.setStyleSheet(
     open("main.qss").read()
 )
-
 main_window.show()
 app.exec_()
